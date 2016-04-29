06e4e40e2494ce8d785bd17964e4296f584bcc02
Robyn Smith <rtasmith05@gmail.com>
2016-04-29 10:41:29 -0400

    User story #1 complete

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
-var timeMin = 25;
-var timeSec = 60;
+var timeMin = 0;
+var timeSec = 3;
+var timerIntervalID = 0;
+
+function pad (str, max) {
+  str = str.toString();
+  return str.length < max ? pad("0" + str, max) : str;
+}
+
 
 function updateTimer() {
+	var displayString = "";
+
+	console.log("Update timer()");
 	if (timeSec === 0) {
-		
+		timeSec = 59;
+		timeMin--;
+	} else {
+		timeSec--;
 	}
-	$('timer').val(timeMin + ":" + timeSec);
+
+	displayString = timeMin + ":" + pad(timeSec, 2);
+
+
+	$(".timer").html(displayString);
+	
+	if (timeMin < 1 && timeSec < 1) {
+		$(".timer").css('color', 'red');
+		clearInterval(timerIntervalID);
+		alert("Pomodoro Over!")
+	}
+}
+
+function test() {
+	console.log("Test");
 }
 
 $(document).ready(function() {
 	$("button").click(function() {
-		setInterval(updateTimer, 1000);
+		console.log("Button pressed");
+		timerIntervalID = setInterval(updateTimer, 1000);
 	});
+});
 
 
 
